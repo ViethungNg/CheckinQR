99091e34017dc7f06d3400ce826a9f9b53640be4
ViethungNg <ngvhungcv@gmail.com>
2026-07-28 14:54:04 +0700

    Test RRR

--- admin/index.php
+++ admin/index.php
@@ -31,7 +31,7 @@ if (isKinhDoanh()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - CheckinQR</title>
+    <title>Dashboardđ - CheckinQR</title>
     <link rel="stylesheet" href="../assets/css/admin-responsive.css?v=<?php echo time(); ?>">
     <style>
         :root {
